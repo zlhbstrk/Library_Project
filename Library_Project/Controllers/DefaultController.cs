841da8add71374170242e49218f6cc8b434bfca2
Zeliha Baştürk <zlhbstrk@hotmail.com>
2020-09-23 22:07:57 +0300

    first commit

---
+++ Library_Project/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library_Project.Models; //*
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Project.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var book = new List<Books>()
+            {
+                new Books(){ID=1, BookName="Satranç", Author="Zweig"},
+                new Books(){ID=2, BookName="Dr Ox'un Deneyi", Author="Jules Verne"},
+                new Books(){ID=3, BookName="Deli Fişek", Author="Vasconceulos"},
+                new Books(){ID=4, BookName="Kırmızı Bisiklet", Author="Muzaffer İzgü"},
+                new Books(){ID=5, BookName="Toros Canavarı", Author="Aziz Nesin"},
+                new Books(){ID=6, BookName="Eşikte Duran İnsan", Author="Rasim Özdenören"}
+            };
+
+            return View(book);
+        }
+    }
+}
---
+++ Library_Project/Models/Books.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Library_Project.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+    }
+}
